4834ce97b297659c90bab10b25b3474c3b590e89
Matthew Allwright <matt.a.4890@gmail.com>
2018-01-25 21:58:12 -0700

    Added basic Eclipse workspace for Assignment 2, and Assignment 1

---
+++ Assignments-Workspace/Assignment2/src/BankAccount.java
@@ -0,0 +1,51 @@
+
+/**
+ * TODO: Class JavaDoc
+ * 
+ * @author T7
+ * @version 0.1
+ *
+ */
+public class BankAccount {
+	
+	//Instance Variables
+	
+	private double balance, overdraftAmount = 100;
+
+	/**
+	 * TODO: Deposit JavaDoc
+	 * 
+	 * @param amount
+	 */
+	public void deposit(double amount) {
+		
+	}
+	
+	/**
+	 * TODO: Withdraw JavaDoc
+	 * 
+	 * @param amount
+	 */
+	public void withdraw(double amount) {
+		
+	}
+	
+	/**
+	 * TODO: Balance Getter JavaDoc
+	 * 
+	 * @param amount
+	 */
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	/**
+	 * TODO: Overdraft Setter JavaDoc
+	 * 
+	 * @param amount
+	 */
+	public void setOverdraftAmount(double amount) {
+		this.overdraftAmount = amount;
+	}
+
+}
