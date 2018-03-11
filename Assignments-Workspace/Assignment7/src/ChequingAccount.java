fca44dd1c641ddeed03ff9d60e3a0baefee3f8f6
Colin Au Yeung <daisuke.moto403@gmail.com>
2018-03-11 14:30:35 -0600

    Assignment 7 should be done, if someone wants to do the trace and check the comments

---
+++ Assignments-Workspace/Assignment7/src/ChequingAccount.java
@@ -0,0 +1,163 @@
+
+public class ChequingAccount extends BankAccount {
+
+	/*
+	 * 
+	 * INSTANCE VARIABLES
+	 * 
+	 */
+
+	private double overdraftFee;
+	private double overdraftAmount = 100.0;
+
+	/*
+	 * 
+	 * CONSTRUCTORS
+	 * 
+	 */
+
+	/**
+	 * Creates a new chequing account with an overdraft fee.
+	 * 
+	 * @param overdraftFee
+	 *            The amount charged to the account every time its balance passes
+	 *            below 0.
+	 */
+	public ChequingAccount(double overdraftFee) {
+
+		super();
+		this.setOverdraftFee(overdraftFee);
+
+	}
+
+	/**
+	 * Creates a new chequing account with an customer, starting balance, and
+	 * overdraft fee.
+	 * 
+	 * @param customer
+	 *            The customer to tie to the bank account.
+	 * @param startingBalance
+	 *            Initial amount in bank account.
+	 * @param overdraftFee
+	 *            The amount charged to the account every time its balance passes
+	 *            below 0.
+	 */
+	public ChequingAccount(Customer customer, double startingBalance, double overdraftFee) {
+
+		super(customer, startingBalance);
+		this.setOverdraftFee(overdraftFee);
+
+	}
+
+	/*
+	 * 
+	 * METHODS
+	 * 
+	 */
+
+	/**
+	 * Sets the overdraft limit for the account.
+	 * 
+	 * @param amount
+	 *            The amount to set the overdraft limit to.
+	 */
+	public void setOverdraftAmount(double amount) {
+
+		// If the amount set will not mean the current balance is overdrafted...
+		// (I know that isn't a word, but you get what I mean :P)
+		if (this.getBalance() + amount >= 0) {
+
+			// Set the amount
+			this.overdraftAmount = amount;
+
+		}
+
+	}
+
+	/**
+	 * Returns the overdraft amount.
+	 * 
+	 * @return The overdraft amount.
+	 */
+	public double getOverdraftAmount() {
+		
+		return this.overdraftAmount;
+		
+	}
+
+	/**
+	 * Returns the overdraft fee.
+	 * 
+	 * @return The overdraft fee.
+	 */
+	public double getOverdraftFee() {
+		
+		return overdraftFee;
+		
+	}
+
+	/**
+	 * Sets the overdraft fee.
+	 * 
+	 * @param overdraftFee The new overdraft fee.
+	 */
+	public void setOverdraftFee(double overdraftFee) {
+		
+		// Make sure the fee is not negative
+		if (overdraftFee >= 0) {
+			this.overdraftFee = overdraftFee;
+		}
+		
+	}
+
+	/**
+	 * Withdraws a specified amount from the account balance, but will not withdraw
+	 * anything if the resulting balance would be below the set overdraft amount.
+	 * 
+	 * @param amount
+	 *            Total amount to withdraw.
+	 */
+	@Override
+	public void withdraw(double amount) {
+
+		// If the amount is positive...
+		if (amount > 0) {
+
+			// Calculate what the balance would be after the withdrawal
+			double oldBalance = this.getBalance();
+			double newBalance = this.getBalance() - amount;
+
+			// If the resulting balance will be above or equal to the overdraft amount...
+			if (newBalance >= (0 - this.overdraftAmount)) {
+
+				// Charge the overdraft fee if the balance drops below 0
+				if (oldBalance >= 0 & newBalance < 0) {
+					newBalance -= this.overdraftFee;
+				}
+
+				// Withdraw the amount
+				this.setBalance(newBalance);
+				System.out.println("Your new balance is $" + Double.toString(newBalance));
+
+			} else {
+
+				// Don't withdraw the amount
+				System.out.println("Can't withdraw: Overdraft detected");
+
+			}
+
+		}
+
+	}
+
+	@Override
+	protected double getMonthlyFeesAndInterest() {
+		if(this.getBalance() >= 0) {
+			return 0;
+		}
+		else {
+			return (0.20*this.getBalance());
+		}
+	}
+
+}
---
+++ Assignments-Workspace/Assignment7/src/Customer.java
@@ -0,0 +1,121 @@
+
+/**
+ * This class represents a customer with a name and an ID number. It is meant to
+ * be used in conjunction with the BankAcount class to tie people with their
+ * bank accounts.
+ * 
+ * @version 1.1.1
+ * @author T07
+ * @see BankAccount
+ */
+public class Customer {
+
+	// INSTANCE VARIABLES
+
+	private String name = "";
+	private int id;
+
+	// CONSTRUCTORS
+
+	/**
+	 * Initialize an empty customer object. (Not actually needed to be defined) <br>
+	 * Caution: May or may not have a soul.
+	 */
+	Customer() {
+
+		// this.soul = null;
+
+	}// End of default constructor
+
+	/**
+	 * Initialize a customer with a name and id number.
+	 * 
+	 * @param name
+	 *            Name of customer.
+	 * @param id
+	 *            Customer identification number.
+	 */
+	Customer(String name, int id) {
+
+		// Initialize name and id as provided
+		this.name = name;
+		this.id = id;
+
+	}// End of copy constructor
+
+	/**
+	 * Create a customer object based off of an existing customer object.
+	 *
+	 * @param customerToCopy
+	 *            The customer to copy all of the information off of.
+	 */
+	Customer(Customer customerToCopy) {
+
+		// Get information through methods to keep private info secure
+		this.name = customerToCopy.getName();
+		this.id = customerToCopy.getID();
+
+	}// End of constructor
+
+	// METHODS
+
+	/**
+	 * Returns the customer name.
+	 * 
+	 * @return The current customer name.
+	 */
+	public String getName() {
+
+		return this.name;
+
+	}// End of getName method
+
+	/**
+	 * Returns the customers ID number.
+	 * 
+	 * @return The current customer ID number.
+	 */
+	public int getID() {
+
+		return this.id;
+
+	}// End of getID method
+
+	/**
+	 * Set customer name.
+	 * 
+	 * @param newName
+	 *            Name to replace current customer name with.
+	 */
+	public void setName(String newName) {
+
+		this.name = newName;
+
+	}// End of setName method
+
+	/**
+	 * Set customer ID.
+	 * 
+	 * @param newID
+	 *            Customer ID number to replace current ID number with.
+	 */
+	public void setID(int newID) {
+
+		this.id = newID;
+
+	}// End of setID method
+
+	/**
+	 * Returns all customer information.
+	 * 
+	 * @return info all customer information
+	 */
+	public String toString() {
+
+		// Format a string with the customer's name and ID.
+		String info = "Customer Name: " + this.name + ", ID Number: " + this.id;
+		return info;
+
+	}// End of toString method
+
+}// End of Customer Class
