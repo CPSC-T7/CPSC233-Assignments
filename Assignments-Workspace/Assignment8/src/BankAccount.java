d39480a65a58a2a363ce1a8c4c6e000ada17014f
Matthew Allwright <matt.a.4890@gmail.com>
2018-03-20 17:30:51 -0600

    Completed Assignment 8

---
+++ Assignments-Workspace/Assignment8/src/BankAccount.java
@@ -0,0 +1,232 @@
+
+/**
+ * This class defines a bank account from which you can get the balance,
+ * deposit, and withdraw, as well as tie a Customer object to it.
+ * 
+ * @version 2.4
+ * @author T07
+ */
+abstract class BankAccount {
+	
+	/*
+	 * 
+	 * INSTANCE VARIABLES
+	 * 
+	 */
+	
+	private double		balance		= 0.0;
+	private Customer	customer	= new Customer();
+	
+	/*
+	 * 
+	 * CONSTRUCTORS
+	 * 
+	 */
+	
+	/**
+	 * Initialize an empty bank account.
+	 */
+	BankAccount() {
+		
+		// Empty
+		
+	}// End of default constructor
+	
+	/**
+	 * Creates a bank account with a customer tied to it.
+	 *
+	 * @param customer
+	 *            The customer to tie to the bank account.
+	 */
+	BankAccount(Customer customer) {
+		
+		this.customer = new Customer(customer);
+		
+	}// End of constructor
+	
+	/**
+	 * Creates a bank account with a customer tied to it, and a starting balance.
+	 *
+	 * @param customer
+	 *            The customer to tie to the bank account.
+	 * @param balance
+	 *            Initial amount in bank account.
+	 */
+	BankAccount(Customer customer, double balance) {
+		
+		this.customer = new Customer(customer);
+		this.balance = balance;
+		
+	}// End of constructor
+	
+	/**
+	 * Creates a bank account with only a starting balance.
+	 * 
+	 * @param initialBalance
+	 *            Initial amount in bank account.
+	 */
+	BankAccount(double initialBalance) {
+		
+		this.balance = initialBalance;
+		
+	}// End of constructor
+	
+	/*
+	 * 
+	 * ABSTRACT METHODS
+	 * 
+	 */
+	
+	/**
+	 * Returns the monthly change in balance. Negative if the balance should go down
+	 * and positive if the balance should increase.
+	 * 
+	 * @return the amount that the balance should change by
+	 */
+	protected abstract double getMonthlyFeesAndInterest();
+	
+	/*
+	 * 
+	 * METHODS
+	 * 
+	 */
+	
+	/**
+	 * Deposits a specified amount into the account balance.
+	 * 
+	 * @param amount
+	 *            Total amount to deposit.
+	 */
+	public void deposit(double amount) {
+		
+		// If the amount is a reasonable number...
+		if (amount > 0 && amount != Double.POSITIVE_INFINITY) {
+			
+			// Deposit the amount into the account
+			this.balance += amount;
+			System.out.println("Your new balance is $" + Double.toString(this.balance));
+			
+		}
+		
+	}// End of deposit method
+	
+	/**
+	 * Withdraws a specified amount from the account balance, but will not withdraw
+	 * anything if the resulting balance would be below 0.
+	 * 
+	 * @param amount
+	 *            Total amount to withdraw.
+	 */
+	public void withdraw(double amount) {
+		
+		// If the amount is positive...
+		if (amount > 0) {
+			
+			// Calculate what the balance would be after the withdrawal
+			double balance = this.balance - amount;
+			
+			// If the resulting balance will be above or equal to 0...
+			if (balance >= 0) {
+				
+				// Withdraw the amount
+				this.balance = balance;
+				System.out.println("Your new balance is $" + Double.toString(balance));
+				
+			} else {
+				
+				// Don't withdraw the amount
+				System.out.println("Can't withdraw: Overdraft detected");
+				
+			}
+			
+		}
+		
+	}// End of withdraw method
+	
+	/**
+	 * Returns the value of the account balance.
+	 * 
+	 * @return balance Total balance of the account.
+	 */
+	public double getBalance() {
+		
+		// Return the balance
+		return (this.balance);
+		
+	}// End of getBalance method
+	
+	/**
+	 * Returns a copy of customer information.
+	 *
+	 * @return A copy customer information.
+	 */
+	public Customer getCustomer() {
+		
+		// Keep customer information secure by returning a copy of the private customer
+		// object.
+		Customer copy = new Customer(customer);
+		return copy;
+		
+	}// End of getCustomer method
+	
+	/**
+	 * Ties a customer object to the bank account.
+	 * 
+	 * @param customer
+	 *            The customer object to tie to the bank account.
+	 */
+	public void setCustomer(Customer customer) {
+		
+		this.customer = customer;
+		
+	}// End of setCustomer method
+	
+	/**
+	 * Transfers a set amount from this account to another account.
+	 * 
+	 * @param amount
+	 *            The amount to transfer.
+	 * @param receivingAccount
+	 *            The account to transfer the money to.
+	 */
+	public void transfer(double amount, BankAccount receivingAccount) {
+		
+		// Record the existing balance
+		double oldBalance = this.balance;
+		
+		// Try to withdraw the amount
+		withdraw(amount);
+		
+		// If the amount was withdrawn...
+		if (this.balance != oldBalance) {
+			
+			// Transfer the money
+			receivingAccount.deposit(amount);
+			
+		}
+		
+	}// End of transfer method
+	
+	/**
+	 * Updates the balance of the account with any increases due to interest or
+	 * decreases due to fees.
+	 */
+	public void monthEndUpdate() {
+		
+		this.setBalance(this.getBalance() + this.getMonthlyFeesAndInterest());
+		
+	}// End of monthEndUpdate method
+	
+	/**
+	 * Sets the balance of the account.
+	 * 
+	 * @param amount
+	 *            The new account balance.
+	 */
+	protected void setBalance(double amount) {
+		
+		this.balance = amount;
+		
+	}// End of setBalance method
+	
+}// End of BankAccount class
---
+++ Assignments-Workspace/Assignment8/src/ChequingAccount.java
@@ -0,0 +1,173 @@
+
+public class ChequingAccount extends BankAccount {
+
+	/*
+	 * 
+	 * INSTANCE VARIABLES
+	 * 
+	 */
+
+	private double overdraftFee;
+	private double overdraftAmount = 100.0;
+
+	/*
+	 * 
+	 * CONSTRUCTORS
+	 * 
+	 */
+
+	/**
+	 * Creates a new chequing account with an overdraft fee.
+	 * 
+	 * @param overdraftFee
+	 *            The amount charged to the account every time its balance passes
+	 *            below 0.
+	 */
+	public ChequingAccount(double overdraftFee) {
+
+		super();
+		this.setOverdraftFee(overdraftFee);
+
+	} // End of constructor
+
+	/**
+	 * Creates a new chequing account with an customer, starting balance, and
+	 * overdraft fee.
+	 * 
+	 * @param customer
+	 *            The customer to tie to the bank account.
+	 * @param startingBalance
+	 *            Initial amount in bank account.
+	 * @param overdraftFee
+	 *            The amount charged to the account every time its balance passes
+	 *            below 0.
+	 */
+	public ChequingAccount(Customer customer, double startingBalance, double overdraftFee) {
+
+		super(customer, startingBalance);
+		this.setOverdraftFee(overdraftFee);
+
+	} // End of constructor
+
+	/*
+	 * 
+	 * METHODS
+	 * 
+	 */
+
+	/**
+	 * Sets the overdraft limit for the account.
+	 * 
+	 * @param amount
+	 *            The amount to set the overdraft limit to.
+	 */
+	public void setOverdraftAmount(double amount) {
+
+		// If the amount set will not mean the current balance is overdrafted...
+		// (I know that isn't a word, but you get what I mean :P)
+		if (this.getBalance() + amount >= 0) {
+
+			// Set the amount
+			this.overdraftAmount = amount;
+
+		}
+
+	} // End of setOverdraftAmount method
+
+	/**
+	 * Returns the overdraft amount.
+	 * 
+	 * @return The overdraft amount.
+	 */
+	public double getOverdraftAmount() {
+		
+		return this.overdraftAmount;
+		
+	} // End of getOverdraftAmount method
+
+	/**
+	 * Returns the overdraft fee.
+	 * 
+	 * @return The overdraft fee.
+	 */
+	public double getOverdraftFee() {
+		
+		return overdraftFee;
+		
+	} // End of getOverdraftFee method
+
+	/**
+	 * Sets the overdraft fee.
+	 * 
+	 * @param overdraftFee The new overdraft fee.
+	 */
+	public void setOverdraftFee(double overdraftFee) {
+		
+		// Make sure the fee is not negative
+		if (overdraftFee >= 0) {
+			this.overdraftFee = overdraftFee;
+		}
+		
+	} // End of setOverdraftFee method
+
+	/**
+	 * Withdraws a specified amount from the account balance, but will not withdraw
+	 * anything if the resulting balance would be below the set overdraft amount.
+	 * 
+	 * @param amount
+	 *            Total amount to withdraw.
+	 */
+	@Override
+	public void withdraw(double amount) {
+
+		// If the amount is positive...
+		if (amount > 0) {
+
+			// Calculate what the balance would be after the withdrawal
+			double oldBalance = this.getBalance();
+			double newBalance = this.getBalance() - amount;
+
+			// If the resulting balance will be above or equal to the overdraft amount...
+			if (newBalance >= (0 - this.overdraftAmount)) {
+
+				// Charge the overdraft fee if the balance drops below 0
+				if (oldBalance >= 0 & newBalance < 0) {
+					newBalance -= this.overdraftFee;
+				}
+
+				// Withdraw the amount
+				this.setBalance(newBalance);
+				System.out.println("Your new balance is $" + Double.toString(newBalance));
+
+			} else {
+
+				// Don't withdraw the amount
+				System.out.println("Can't withdraw: Overdraft detected");
+
+			}
+
+		}
+
+	} // End of withdraw method
+
+	// JavaDoc set by parent.
+	@Override
+	protected double getMonthlyFeesAndInterest() {
+		
+		//If the balance is positive return 0
+		if(this.getBalance() >= 0) {
+			
+			return 0;
+			
+		}
+		
+		//Otherwise return 20% of the negative balance
+		else {
+			
+			return (0.20*this.getBalance());
+			
+		}
+		
+	}// End of monthEndUpdate method
+
+}
